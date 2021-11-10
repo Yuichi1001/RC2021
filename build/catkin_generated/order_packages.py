ccbca19968d08596362ac4239db30ce8aedabd0e
chenhongyu <913637919@qq.com>
2021-11-10 19:22:19 +0800

    初始化工作空间

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/qudoudou/RC2021/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/qudoudou/cv_bridge/install;/opt/ros/melodic'.split(';') if '/home/qudoudou/cv_bridge/install;/opt/ros/melodic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/qudoudou/RC2021/devel:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH='/home/qudoudou/cv_bridge/install/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig:/usr/local/lib/pkgconfig'
+export PWD='/home/qudoudou/RC2021/build'
+export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages:/home/qudoudou/cv_bridge/install/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/qudoudou/RC2021/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/qudoudou/RC2021/src:$ROS_PACKAGE_PATH"
